f04fa94f77fbce05c41f50dcc28f6fc7627c3731
Raymond Cox <raymondc1911@gmail.com>
2022-12-12 15:10:54 -0500

    Fix builds

--- webpack.common.js
+++ webpack.common.js
@@ -2,34 +2,25 @@ const Webpack = require("webpack");
 module.exports = {
   entry: {
     vendor: "./src/vendor.js",
-    main: "./src/index.js"
+    main: "./src/index.js",
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ["html-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "assets",
-              esModule: false
-            }
-          }
-        ]
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   // This config allows to use jQuery $ sign
   plugins: [
     new Webpack.ProvidePlugin({
       $: "jquery",
-      jQuery: "jquery"
-    })
-  ]
+      jQuery: "jquery",
+    }),
+  ],
 };
